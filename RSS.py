0b2e6abbb760952b5312ef5fe3033ea8b3224180
FintechevBocconi <38435954+FintechevBocconi@users.noreply.github.com>
2018-04-16 19:42:37 +0200

    Update RSS.py

--- RSS.py
+++ RSS.py
@@ -20,7 +20,7 @@ def print_rssfeed_yahoo(ticker): #added yahoo name
     
 print_rssfeed_yahoo(ticker="BTC-USD")
 
-def print_rssfeed_bitnewz(detail):
+def print_rssfeed_bitnewz(detail): #printing news from bitnewz
     reply = input("Do you want general (G) or specific (S) news about cryptocurrencies?")
     if reply.lower = "g":
         url_="http://www.bitnewz.net/rss/"
@@ -47,9 +47,70 @@ def print_rssfeed_bitnewz(detail):
                   counter=counter+1
                   print(str(counter)+")" + str(i[0].text))
             elif reply2 = 2:
-                a = input("What term do you want to search?")
-                
+                d = input("What term do you want to search?")
+                url_="http://www.bitnewz.net/rss/feed/"+d
+                urlretrieve(url_,"news/data.xml")
+                root=ET.parse("news/data.xml").getroot()
+                iter=root.iter("item")
+                counter=0
+                for i in iter:
+                  counter=counter+1
+                  print(str(counter)+")" + str(i[0].text))
+           else:
+                e = input("How many results do you want?")
+                url_="http://www.bitnewz.net/rss/feed/"+e
+                urlretrieve(url_,"news/data.xml")
+                root=ET.parse("news/data.xml").getroot()
+                iter=root.iter("item")
+                counter=0
+                for i in iter:
+                  counter=counter+1
+                  print(str(counter)+")" + str(i[0].text))          
        except:
             print("You're supposed to enter either 1 or 2 or 3. Try again")
             reply2 = input("Do you want a specific date (1), search term (2) or number of results (3)?")
-        
+                if reply.lower = "g":
+        url_="http://www.bitnewz.net/rss/"
+        urlretrieve(url_,"news/data.xml")
+        root=ET.parse("news/data.xml").getroot()
+        iter=root.iter("item")
+        counter=0
+        for i in iter:
+           counter=counter+1
+           print(str(counter)+")" + str(i[0].text))
+    else:
+       reply2 = input("Do you want a specific date (1), search term (2) or number of results (3)?")
+       try:
+            if reply2 = 1:
+                a= input("day?")
+                b= input("month?")
+                c= input("year?")
+                url_="http://www.bitnewz.net/rss/feed/"+b+"-"+a+"-"+c
+                urlretrieve(url_,"news/data.xml")
+                root=ET.parse("news/data.xml").getroot()
+                iter=root.iter("item")
+                counter=0
+                for i in iter:
+                  counter=counter+1
+                  print(str(counter)+")" + str(i[0].text))
+            elif reply2 = 2:
+                d = input("What term do you want to search?")
+                url_="http://www.bitnewz.net/rss/feed/"+d
+                urlretrieve(url_,"news/data.xml")
+                root=ET.parse("news/data.xml").getroot()
+                iter=root.iter("item")
+                counter=0
+                for i in iter:
+                  counter=counter+1
+                  print(str(counter)+")" + str(i[0].text))
+           else:
+                e = input("How many results do you want?")
+                url_="http://www.bitnewz.net/rss/feed/"+e
+                urlretrieve(url_,"news/data.xml")
+                root=ET.parse("news/data.xml").getroot()
+                iter=root.iter("item")
+                counter=0
+                for i in iter:
+                  counter=counter+1
+                  print(str(counter)+")" + str(i[0].text))  
+         
